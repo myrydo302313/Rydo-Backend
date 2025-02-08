4d1c79f8c35dad01cb2a2d5f8b8627f21e3e23f8
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-02-08 13:32:53 +0530

    cors link update

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  // origin: "https://www.evotto.in",
-  origin: "http://localhost:5176",
+  origin: "https://rydo.vercel.app/",
+  // origin: "http://localhost:5176",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
