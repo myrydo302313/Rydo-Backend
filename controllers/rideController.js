f5969583be854fde5d142a52f7d710746979f30d
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-03-10 19:18:32 +0530

    cors chang

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -39,7 +39,6 @@ module.exports.createRide = async (req, res) => {
       .findOne({ _id: ride._id })
       .populate("user");
 
-    console.log('ye h captains',rideWithUser)
     console.log('user ye h',user)
 
     captainsInRadius.map((captain) => {
