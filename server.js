893299a791e6c9e2fa6c6d4de2fb602608ee309b
Yuvraj Preet <yuvraj13preet@gmail.com>
2025-03-17 08:56:23 +0530

    Forgot password added

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ initializeSocket(server);
 // Start Server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port : ${PORT}`);
 });
